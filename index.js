1efca9dc0d91483be11347940ca085459871d4da
Adam-OC <adam.osorio-castillo25@compscihigh.org>
2025-03-12 14:49:18 +0000

    done done

--- index.js
+++ index.js
@@ -24,9 +24,24 @@ const Student = mongoose.model("Student", studentSchema, "Students")
 
 // create a route hanlder for /g12 that returns every student in grade 12
 
+app.get("/g12", async (req,res) =>{ 
+  const students = await Student.find({grade: 12})
+  res.json(students)
+}) 
+
 // (OYO) create a route hanlder for /me that returns yourself without using your name in the query
 
+app.get("/me", async (req,res) => {
+  const Adam = await Student.findOne({grade:12},{favSub:"APCalc"})
+  res.json(Adam)
+})
+
 // (OYO) create a route hanlder for /friend that returns someone at your table using their name in the query
+
+app.get("/friend", async (req,res) =>{
+  const friend = await Student.findOne({name:"Oscar"})
+  res.json(friend)
+})
 
 // Write an async function called startServer
 // inside make sure to connect to mongoose w/ your SRV string
@@ -35,13 +50,13 @@ const Student = mongoose.model("Student", studentSchema, "Students")
 // (OYO) save 2 more documents about students at your table
 // make sure to start your server 
 async function startServer() {
-  await mongoose.connect("mongodb+srv://SE12:CSH2025@adamo8.b6ydo.mongodb.net/?retryWrites=true&w=majority&appName=AdamO8")
+  await mongoose.connect("mongodb+srv://SE12:CSH2025@adamo8.b6ydo.mongodb.net/CSHStudents?retryWrites=true&w=majority&appName=AdamO8")
 
-  const me = await new Student({
-    name: "Adam",
-    grade: 12,
-    favSub: "ApCalc"
-  })
+  // const me = await new Student({
+  //   name: "Denisse",
+  //   grade: 12,
+  //   favSub: "Journalism"
+  // }).save()
 
 
   app.listen(3000, () => {
